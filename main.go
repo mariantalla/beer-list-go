3d1cb189821386a98de635182f3d14c3dd3b30c2
Jake Klein <jklein@pivotal.io>
2016-11-22 19:03:34 +0000

    wip post beers

--- main.go
+++ main.go
@@ -19,20 +19,12 @@ type Env struct {
 	Beers         map[string]string
 }
 
-var env *cfenv.App
+var (
+	env *cfenv.App
+	db  *sql.DB
+)
 
 func main() {
-	http.HandleFunc("/", Index)
-	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
-}
-
-func Index(w http.ResponseWriter, req *http.Request) {
-	beers := map[string]string{
-		"awesome-place": "awesome-beer",
-	}
-
-	// beers := make(map[string]string)
-	//
 	env, _ = cfenv.Current()
 	mysqlService, err := env.Services.WithName("db")
 	check(err)
@@ -40,27 +32,29 @@ func Index(w http.ResponseWriter, req *http.Request) {
 	credentials := mysqlService.Credentials
 
 	fmt.Println(strings.Split(credentials["uri"].(string), "//")[1])
-	db, err := sql.Open("mysql", "CLQnXGd1Yoe72dME:JPVgiVLipd5FtFcq@tcp(mysql-broker.local.pcfdev.io:3306)/cf_6a53c48b_291b_405f_86e8_cbb21352f660")
+	db, err = sql.Open("mysql", "CLQnXGd1Yoe72dME:JPVgiVLipd5FtFcq@tcp(mysql-broker.local.pcfdev.io:3306)/cf_6a53c48b_291b_405f_86e8_cbb21352f660")
 	check(err)
 	defer db.Close()
 
 	_, err = db.Exec("create table if not exists beers (id integer PRIMARY KEY AUTO_INCREMENT, region varchar(255) NOT NULL, brand varchar(255) NOT NULL)")
 	check(err)
-	// _, err = db.Exec("insert into beers (region, brand) values ('db-region', 'db-beer')")
-	// check(err)
+
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/create", Create)
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
+}
+
+func Index(w http.ResponseWriter, req *http.Request) {
+	beers := make(map[string]string)
 
 	rows, err := db.Query("select region, brand from beers")
 	check(err)
 
-	fmt.Println("got some results supposedly")
-
 	for rows.Next() {
-		fmt.Println("oh dear")
-		var region sql.NullString
-		var brand sql.NullString
+		var region string
+		var brand string
 		check(rows.Scan(&region, &brand))
-		fmt.Printf("new row: %s %s", region.String, brand.String)
-		beers[region.String] = brand.String
+		beers[region] = brand
 	}
 
 	customEnv := Env{
@@ -69,6 +63,17 @@ func Index(w http.ResponseWriter, req *http.Request) {
 		Beers:         beers,
 	}
 	render(w, "views/index.html", customEnv)
+}
+
+func Create(w http.ResponseWriter, req *http.Request) {
+	check(req.ParseForm())
+	brand := req.PostForm.Get("brand")
+	region := req.PostForm.Get("region")
+
+	_, err := db.Exec(fmt.Sprintf("insert into beers (region, brand) values ('%s', '%s')", region, brand))
+	check(err)
+
+	http.Redirect(w, req, "/", http.StatusFound)
 }
 
 func render(w http.ResponseWriter, tmpl string, env Env) {
