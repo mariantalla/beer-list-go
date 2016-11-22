f1c0d19331a24e52d767292a7415f4df403ed209
Maria Ntalla <mntalla@pivotal.io>
2016-11-22 12:51:35 +0000

    wip adding db

--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Env struct {
-	GATag string
+	GATag         string
 	InstanceIndex string
+	Beers         map[string]string
 }
 
 func main() {
+
+	db, err := sql.Open("mysql",
+		"3AG4rpTntTHg4z96:S5paRHY1Ez5EaI6f@mysql-broker.local.pcfdev.io:3306/cf_ce575afb_bec6_4a80_83cb_de4c1d60c543")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	http.HandleFunc("/", Index)
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
 }
 
 func Index(w http.ResponseWriter, req *http.Request) {
+	beers := map[string]string{
+		"awesome-place": "awesome-beer",
+	}
 	env := Env{
-		GATag: os.Getenv("GA_TAG"),
+		GATag:         os.Getenv("GA_TAG"),
 		InstanceIndex: os.Getenv("INSTANCE_INDEX"),
+		Beers:         beers,
 	}
 	render(w, "views/index.html", env)
 }
