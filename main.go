3cdcc3a9d971c8df041fc38eb4baa65859b966e0
Maria Ntalla <mntalla@pivotal.io>
2016-11-23 08:35:48 +0000

    step2-config from the environment

--- main.go
+++ main.go
@@ -2,11 +2,46 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"os"
 )
 
+type Env struct {
+	GATag         string
+	InstanceIndex string
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./views")))
+	renderCssAndImages()
+	http.HandleFunc("/", Index)
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
+}
+
+func Index(w http.ResponseWriter, req *http.Request) {
+	env := Env{
+		GATag:         os.Getenv("GA_TAG"),
+		InstanceIndex: os.Getenv("INSTANCE_INDEX"),
+	}
+	render(w, "views/index.html", env)
+}
+
+func render(w http.ResponseWriter, htmlTemplate string, env Env) {
+	htmlTemplate = fmt.Sprintf("%s", htmlTemplate)
+	template, err := template.ParseFiles(htmlTemplate)
+	check(err)
+
+	err = template.Execute(w, env)
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func renderCssAndImages() {
+	http.Handle("/views/", http.StripPrefix("/views/", http.FileServer(http.Dir("views"))))
 }
