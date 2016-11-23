b0797b4118468a888d9cc3b72613d066ab4b83a9
Maria Ntalla <mntalla@pivotal.io>
2016-11-23 13:26:29 +0000

    step3-add a database
    And remember to call it 'db' else things go wrong

--- main.go
+++ main.go
@@ -1,28 +1,91 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+
+	cfenv "github.com/cloudfoundry-community/go-cfenv"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Env struct {
 	GATag         string
 	InstanceIndex string
+	Beers         map[string]string
 }
 
+var (
+	env *cfenv.App
+	db  *sql.DB
+	err error
+)
+
 func main() {
+	db = connectToDB()
+	defer db.Close()
 	renderCssAndImages()
 	http.HandleFunc("/", Index)
+	http.HandleFunc("/create", Create)
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
 }
 
+func connectToDB() *sql.DB {
+	env, _ = cfenv.Current()
+	mysqlService, err := env.Services.WithName("db")
+	check(err)
+
+	credentials := mysqlService.Credentials
+
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		credentials["username"].(string),
+		credentials["password"].(string),
+		credentials["hostname"].(string),
+		int(credentials["port"].(float64)),
+		credentials["name"].(string),
+	)
+	db, err = sql.Open("mysql", dns)
+	check(err)
+
+	_, err = db.Exec("create table if not exists beers (id integer PRIMARY KEY AUTO_INCREMENT, region varchar(255) NOT NULL, brand varchar(255) NOT NULL)")
+	check(err)
+	return db
+}
+
+func Create(w http.ResponseWriter, req *http.Request) {
+	check(req.ParseForm())
+	brand := req.PostForm.Get("brand")
+	region := req.PostForm.Get("region")
+	fmt.Printf("creating beer: %s/%s\n", region, brand)
+
+	_, err := db.Exec(fmt.Sprintf("insert into beers (region, brand) values ('%s', '%s')", region, brand))
+	check(err)
+
+	http.Redirect(w, req, "/", http.StatusFound)
+}
+
 func Index(w http.ResponseWriter, req *http.Request) {
+	beers := make(map[string]string)
+
+	rows, err := db.Query("select region, brand from beers")
+	check(err)
+
+	for rows.Next() {
+
+		var region string
+		var brand string
+		check(rows.Scan(&region, &brand))
+		fmt.Printf("found beer: %s/%s\n", region, brand)
+		beers[region] = brand
+	}
+
 	env := Env{
 		GATag:         os.Getenv("GA_TAG"),
 		InstanceIndex: os.Getenv("INSTANCE_INDEX"),
+		Beers:         beers,
 	}
 	render(w, "views/index.html", env)
 }
